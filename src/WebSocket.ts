7599c89f7c8906b4bbcd6f54530e021154bfe262
JF <simplyhiphopmusic@gmail.com>
2022-03-19 17:08:01 -0400

    -

--- src/WebSocket.ts
+++ src/WebSocket.ts
@@ -6,51 +6,57 @@ import { WebSocketServer } from 'ws'
 import open from 'open'
 import type { WebSocket } from 'ws'
 
-
+type ChannelController = {
+	promise?: Promise<unknown>,
+	resolve?: (value?: unknown) => void,
+	reject?: (value?: unknown) => void,
+}
 
 export default class WebSocketsConnection implements Connection {
 	private _url: URL
 	private _serverPort: number
 	private _wss?: WebSocketServer
 	private _ws?: WebSocket
+	private _channelController: ChannelController = {}
 	private _promiseController = new PromiseController()
 
 	constructor(connectToUrl: string | URL, appInfo?: AppInfo, serverPort?: number) {
+		this._serverPort = serverPort || 1985
 		this._url = generateUrl(connectToUrl)
 		const urlInfo = {
-			origin: window.location.origin,
+			origin: 'ws://localhost:' + this._serverPort,
 		} as any
 		if (appInfo?.name) { urlInfo.name = appInfo.name }
 		if (appInfo?.logo) { urlInfo.logo = appInfo.logo }
 		this._url.hash = new URLSearchParams(urlInfo).toString()
-		this._serverPort = serverPort || 1985
 	}
 
 	connect() {
 		if (this._wss) { return }
-		open(this._url.href)
+		
+		const promise = new Promise((resolve, reject) => this._channelController = { resolve, reject })
+		this._channelController.promise = promise
+		
 		this._wss = new WebSocketServer({ port: this._serverPort })
 		this._wss.on('connection', ws => {
 			if (this._ws) { return }
 			this._ws = ws
 			ws.on('message', data => {
 				const message = JSON.parse(data.toString())
-				if (typeof data !== 'object') { return }
-				const { method, params, id, result, error, session } = message as { [key: string]: unknown }
 				console.log('received: %s', message)
+				if (typeof data !== 'object') { return }
 				if (this._promiseController.processResponse(message)) { return }
+				const { method, params, id, result, error, session } = message as { [key: string]: unknown }
 				if (typeof method !== 'string') { return }
-				// reserved methods
-				if (method === 'ready') {
-					// todo
-					return
-				}
-				if (method === 'change') { return }
+				if (method === 'connect') { this._channelController?.resolve?.() }
 			})
 		})
+		
+		open(this._url.href)
 	}
 
 	disconnect() {
+		this._channelController?.reject?.()
 		this._wss?.close()
 		this._ws?.close()
 		this._wss = undefined
@@ -59,6 +65,15 @@ export default class WebSocketsConnection implements Connection {
 
 	postMessage(method: string, params?: any[], options?: PostMessageOptions & ProtocolInfo) {
 		if (!this._ws) { throw 'no connection' }
-		this._ws.send('something')
+		const message = { method, params, protocol: options?.protocol, version: options?.version, jsonrpc: '2.0' }
+		const promise = this._promiseController.newMessagePromise(message, options)
+		this.deliverMessage(message, options)
+		return promise
+	}
+	
+	private deliverMessage (message: any, options?: PostMessageOptions) {
+		this._channelController.promise = this._channelController?.promise
+			?.then(() => this._ws?.send(JSON.stringify(message)))
+			.catch(() => { return })
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { ArweaveApi } from 'arweave-wallet-connector/lib/Arweave.js'
 import WebSockets from './WebSocket.js'
 
-export const ArweaveWebSockets: any = ArweaveApi(WebSockets)
+export const ArweaveWebSockets = ArweaveApi(WebSockets)
+
+const test = new ArweaveWebSockets('http://localhost:8080')
+test.connect()
+setInterval(() => test.getArweaveConfig().then(res => console.log(res)), 10000)
